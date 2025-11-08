f65b3cbff7b74c63ed4b1be2a61859f5f60ad145
Jozsef Geng <dev@jozsefgeng.com>
2025-11-08 16:32:33 +0100

    feat: add price tax hint

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -8,4 +8,5 @@ global $product;
 ?>
 <div class="py-2.5 font-bold text-2xl text-color-neutral-100">
       <?php echo $product->get_price_html(); ?></p>
+      <span class="block font-normal text-xs"><?php echo __('including VAT, plus shipping costs')?></span>
 </div>
